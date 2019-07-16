69033437f841db3aba3ea7e62f5cd73380a037ae
Kamil Lepek <kamil.lepek94@gmail.com>
2019-07-16 15:38:33 +0200

    Create basic documentation
    Signed-off-by: Kamil Lepek <kamil.lepek94@gmail.com>

--- config/configuration_example.py
+++ config/configuration_example.py
@@ -3,7 +3,13 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
-
+# Defines path to open cas repo
 opencas_repo_path = '/path/to/opencas/repo'
-test_wrapper_dir = '/path/to/test/wrapper'
+
+# Test wrapper can be used in order to wrap some behaviours with custom features.
+# By default (set to blank string) it is disabled. Use with caution
+test_wrapper_dir = ''
+
+# This command will be executed before every command that might require a proxy
+# Example use case: exporting proxy in order to access external content
 proxy_command = ''
--- config/example_dut_config.py
+++ config/example_dut_config.py
@@ -3,11 +3,14 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
+from test_utils.size import Size, Unit
+
+
 ip = "x.x.x.x"
 disks = [{'path': '/dev/device_name',
-          'serial': 'ABC',
-          'type': 'nand',
-          'size': 100,
-          'blocksize': 512}]  # both block size and disk size are given in bytes
+          'serial': 'ABC',  # disk serial number
+          'type': 'nand',  # disk_type
+          'size': Size(10, Unit.GibiByte),
+          'blocksize': Size(1, Unit.Blocks512)}]
 user = "example_user"
 password = "example_password"
