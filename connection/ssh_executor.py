c5bac99e9fe1c33110acd07a606e0f28d17ce50c
Daniel Madej <daniel.madej@intel.com>
2019-07-24 17:48:50 +0200

    Workaround for SSH hanging on reading line with unusual ending IPMI commands have this problem
    Signed-off-by: Daniel Madej <daniel.madej@intel.com>

--- connection/ssh_executor.py
+++ connection/ssh_executor.py
@@ -4,11 +4,13 @@
 #
 
 import re
+import time
 from datetime import timedelta
 
 import paramiko
 
 from connection.base_executor import BaseExecutor
+from test_package.test_properties import TestProperties
 from test_utils.output import Output
 
 
@@ -19,8 +21,8 @@ class SshExecutor(BaseExecutor):
         self.connect(username, password, port)
         self.channel = self.ssh.invoke_shell()
         self.stdin = self.channel.makefile('wb')
-        self.stdout = self.channel.makefile('r')
-        self.execute("")  # empty command is sent to initialize and check connection
+        self.stdout = self.channel.makefile('rb')
+        self.execute("", timedelta(seconds=3))  # empty command to initialize and check connection
 
     def __del__(self):
         self.ssh.close()
@@ -46,19 +48,47 @@ class SshExecutor(BaseExecutor):
         self.stdin.flush()
         result = Output([], "", 0)
 
-        for line in self.stdout:
-            if line.startswith(f'__exit_code:'):
-                result.exit_code = int(str(line).split()[-1])
-                break
-            elif str(line).startswith(command) or str(line).startswith(echo_cmd):
-                result.stdout = []
-            elif '__exit_code' not in line and \
-                    not line.replace(' \r', '').strip().endswith(command):
-                result.stdout.append(
-                    re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line)
-                    .replace('\b', '')
-                    .replace('\r', '')
-                    .replace('\n', ''))
+        start = time.time()
+
+        n_bytes = 0
+        exit_code_received = False
+        echo_cmd_displayed = False
+        while not exit_code_received and time.time() - start < timeout.seconds:
+            n_bytes_next = len(self.stdout.channel.in_buffer)
+            if n_bytes_next == 0:
+                continue
+            if n_bytes_next > n_bytes:
+                n_bytes = n_bytes_next
+                time.sleep(0.01)
+                continue
+
+            stdout = self.stdout.read(n_bytes)
+            for line in stdout.splitlines():
+                line = line.decode("utf-8")
+                if re.match(r'__exit_code: \d+', line):
+                    result.exit_code = int(line.split()[-1])
+                    exit_code_received = True
+                    break
+                elif line.endswith(echo_cmd):
+                    echo_cmd_displayed = True
+                elif command == "" and line == "":
+                    continue
+                elif line.endswith(command):
+                    result.stdout = []
+                elif '__exit_code' not in line and \
+                      not line.replace(' \r', '').strip().endswith(command):
+                    result.stdout.append(
+                        re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]').sub('', line)
+                            .replace('\b', '')
+                            .replace('\r', '')
+                            .replace('\n', ''))
+            n_bytes = 0
+
+        if not exit_code_received:
+            if echo_cmd_displayed:
+                TestProperties.LOGGER.warn("No exit code received")
+            else:
+                raise TimeoutError(f"SSH command '{command}' has timed out after {timeout}")
 
         if result.stdout:
             result.stdout = '\n'.join(result.stdout)
