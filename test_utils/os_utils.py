f4e146ae61bc819eb27e8d5c75e7ab94604043d2
Michal Mielewczyk <michal.mielewczyk@intel.com>
2019-07-30 07:30:05 -0400

    Extendet os utils with 'sync'.
    Signed-off-by: Michal Mielewczyk <michal.mielewczyk@intel.com>

--- test_utils/os_utils.py
+++ test_utils/os_utils.py
@@ -4,6 +4,7 @@
 #
 
 import time
+from test_package.test_properties import TestProperties
 
 
 def wait(predicate, timeout, interval=None):
@@ -16,3 +17,10 @@ def wait(predicate, timeout, interval=None):
         if interval is not None:
             time.sleep(interval)
     return result
+
+
+def sync():
+    output = TestProperties.executor.execute("sync")
+    if output.exit_code != 0:
+        raise Exception(
+            f"Sync command failed. stdout: {output.stdout} \n stderr :{output.stderr}")
