abee5688c17be7da4e65d325e45e24e9965a7843
Katarzyna Lapinska <katarzyna.lapinska@intel.com>
2019-08-29 13:59:36 +0200

    Add method refreshing file object properties

--- test_utils/filesystem/fs_item.py
+++ test_utils/filesystem/fs_item.py
@@ -36,12 +36,8 @@ class FsItem:
         return self.full_path
 
     def chmod_numerical(self, permissions: int, recursive: bool = False):
-        if fs_utils.chmod_numerical(self.full_path, permissions, recursive):
-            self.permissions.user = fs_utils.Permissions(int(str(permissions)[0]))
-            if len(str(permissions)) > 1:
-                self.permissions.group = fs_utils.Permissions(int(str(permissions)[1]))
-            if len(str(permissions)) > 2:
-                self.permissions.other = fs_utils.Permissions(int(str(permissions)[2]))
+        fs_utils.chmod_numerical(self.full_path, permissions, recursive)
+        self.refresh_item()
 
     def chmod(self,
               permissions: fs_utils.Permissions,
@@ -50,12 +46,19 @@ class FsItem:
               recursive: bool = False):
         fs_utils.chmod(self.full_path, permissions, users, sign=sign, recursive=recursive)
         output = fs_utils.ls(f"{self.full_path}")
-        self.permissions = fs_utils.parse_ls_output(output)[0].permissions
+        self.refresh_item()
 
     def chown(self, owner, group, recursive: bool = False):
         fs_utils.chown(self.full_path, owner, group, recursive)
-        self.owner = owner
-        self.group = group
+        self.refresh_item()
+
+    def refresh_item(self):
+        updated_file = fs_utils.parse_ls_output(fs_utils.ls(self.full_path))[0]
+        self.size = updated_file.size
+        self.permissions = updated_file.permissions
+        self.modification_time = updated_file.modification_time
+        self.owner = updated_file.owner
+        self.group = updated_file.group
 
 
 class FsPermissions:
@@ -63,3 +66,6 @@ class FsPermissions:
         self.user = user
         self.group = group
         self.other = other
+
+    def __eq__(self, other):
+        return self.user == other.user and self.group == other.group and self.other == other.other
