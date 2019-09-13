d371381ab8e1c69f291b54db5b03514bc8bb0c2c
Kamil Lepek <kamil.lepek94@gmail.com>
2019-09-13 14:56:02 +0200

    Fix for invalid data type in example_dut_config
    Signed-off-by: Kamil Lepek <kamil.lepek94@gmail.com>

--- config/example_dut_config.py
+++ config/example_dut_config.py
@@ -3,14 +3,11 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
-from test_utils.size import Size, Unit
-
 
 ip = "x.x.x.x"
 disks = [{'path': '/dev/device_name',
           'serial': 'ABC',  # disk serial number
           'type': 'nand',  # disk_type
-          'size': Size(10, Unit.GibiByte),
-          'blocksize': Size(1, Unit.Blocks512)}]
+          'blocksize': 512}]  # 512B
 user = "example_user"
 password = "example_password"
