d392afba1eac615ed8f2025f5cf4aedf025a908e
Robert Baldyga <robert.baldyga@intel.com>
2019-10-17 14:21:08 +0200

    Remove cas_init_config_path from config
    In Open CAS Linux this path if fixed and user has no way to change it,
    so there is no need to parametrize it via config.
    
    Signed-off-by: Robert Baldyga <robert.baldyga@intel.com>

--- config/configuration_example.py
+++ config/configuration_example.py
@@ -9,5 +9,3 @@ opencas_repo_path = '/path/to/opencas/repo'
 # Test wrapper can be used in order to wrap some behaviours with custom features.
 # By default (set to blank string) it is disabled. Use with caution
 test_wrapper_dir = ''
-
-cas_init_config_path = "/etc/opencas/opencas.conf"
