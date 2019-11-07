d160a4b79d105dd793af87f0c50c7e4b6b4a694f
Katarzyna Lapinska <katarzyna.lapinska@intel.com>
2019-11-07 13:36:59 +0100

    Close all test iterations in one named test group

--- core/test_run.py
+++ core/test_run.py
@@ -23,12 +23,14 @@ class TestRun:
         return cls.LOGGER.group(message)
 
     @classmethod
-    def iteration(cls, iterable):
+    def iteration(cls, iterable, group_name=None):
+        TestRun.LOGGER.start_group(f"{group_name}" if group_name is not None else "Iteration list")
         items = list(iterable)
         for i, item in enumerate(items, start=1):
             cls.LOGGER.start_iteration(f"Iteration {i}/{len(items)}")
             yield item
             TestRun.LOGGER.end_iteration()
+        TestRun.LOGGER.end_group()
 
     @classmethod
     def fail(cls, message):
